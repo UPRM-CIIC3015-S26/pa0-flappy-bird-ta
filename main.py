55ed7ac74eae1ea407e5cb3ae83506bec46d11ab
Tu Nombre <tu_email@upr.edu>
2026-02-09 22:00:02 -0400

    Task 6: Change name variable and debug

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ running = True
 while running:
     # TODO 6: Changing the name!
     # D'oh! This is not yout name isn't follow the detailed instructions on the PDF to complete this task.
-    name = "Homer Simpson"
+    name = "Yalimarie Perez"
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
